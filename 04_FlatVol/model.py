e2868112f6c17e9f169f81245dcbbc7bb4fd4b7d
Nikhil Gowda <114958232+Nick3344@users.noreply.github.com>
2024-03-10 22:53:27 +0100

    Add files via upload

---
+++ 04_FlatVol/model.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from market_data import *
+from enums import *
+import numpy as np
+
+
+# TASK:
+# Implement BSVol and FlatVol models in the pricing library
+
+
+class MarketModel(ABC):
+    def __init__(self, underlying: Stock) -> None:
+        self.underlying: Stock = underlying
+        self.risk_free_rate: float = MarketData.get_risk_free_rate()
+        self.spot: float = MarketData.get_spot()[self.underlying]
+        self.volgrid: VolGrid = MarketData.get_volgrid()[self.underlying]
+
+    def bump_rate(self, bump_size: float) -> None:
+        self.risk_free_rate += bump_size
+
+    def bump_spot(self, bump_size: float) -> None:
+        self.spot += bump_size
+
+    def bump_volgrid(self, bump_size: float) -> None:
+        self.volgrid.values += bump_size
+
+    def calc_df(self, tenor: float) -> float:
+        return np.exp(-1.0 * self.risk_free_rate * tenor)
+
+    @abstractmethod
+    def get_vol(self, strike: float, expiry: float) -> float:
+        pass
+class BSVol(MarketModel):
+    define __init__(self,Stock) -> None :
+    super().
+
+    def get_vol(self,strike:float,expiry:float) -> float:
+        expiry = 1.9
+        strike = 1.9 *self.spot
+        coordinate = np.array(([strike,expiry))
+        return self.volgrid.get_vol(coordinate)[0]
+class FlatVolModel(MarketModel):
+    define
+    __init__(self, Stock) -> None:
+    super().
+
+    def get_vol(self,strike:float,expiry:float) -> float:
+        coordinate = np.array(([strike, expiry))
+        return self.volgrid.get_vol(coordinate)[0]
